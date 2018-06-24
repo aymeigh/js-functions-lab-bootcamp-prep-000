94c98ff284ad38ecf207f3f5d244ea0286fcb1a7
Amy Zhou <aymeighz@gmail.com>
2018-06-24 03:56:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays(string) {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-  return "Happy holidays, `${name}!`"
+  return "Happy holidays, `${name}!`";
 }
 function happyHolidayTo(holiday, name) {
-  return "Happy `${holiday}`, `${name}!`"
+  return "Happy `${holiday}`, `${name}!`";
 }
